145907f2fcaceb26136b8044eb8db70f80979920
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-04-18 14:26:58 +0700

    update

---
+++ projects/alpha-root/src/app/_models/app/list/User.ts
@@ -0,0 +1,19 @@
+export class User {
+  id?: string;
+  groupId?: number;
+  userName?: string;
+  fullName?: string;
+  employeeId?: any;
+  employeeCode?: string;
+  employeeName?: string;
+  groupName?: string;
+  email?: string;
+  avatar?: string;
+  password?: string;
+  rePassword?: string;
+  lock?: boolean = false;
+  isWebapp?: boolean = false;
+  isPortal?: boolean = false;
+  isMobile?: boolean = false;
+  fmcToken?: string;
+}
